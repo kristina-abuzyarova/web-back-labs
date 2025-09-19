50a1bd3f3f2a03509ef31bd733b2c97dbab92c5c
Абузярова Кристина <kristina19283746@gmail.com>
2025-09-19 23:24:31 +0700

    Служебная информация

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -46,10 +47,17 @@ def image():
 def counter():
     global count
     count +=1
+    time = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    url = request.url
+    client_ip = request.remote_addr
     return '''
 <!doctype html>
 <html>
     <body>
         Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <hr>
+        Дата и время: ''' + time + ''' <br>
+        Запрошенный адрес: ''' + url + ''' <br>
+        Ваш IP-адрес: ''' + client_ip + ''' <br>
     </body>
 </html>'''

8413e4be7298c8bae60639f59a27807f546ab808
Абузярова Кристина <kristina19283746@gmail.com>
2025-09-19 19:41:29 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<!doctype html>"
         "<html>"
@@ -9,3 +10,20 @@ def start():
         "       <h1>web-сервер на flask</h1>"
         "   </body>"
         "</html>"""
+
+@app.route("/author")
+def author():
+    name = "Абузярова Кристина Руслановна"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+
+        <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""

cb322aeb0e20919382ee817e7fe92b18e61c7174
Абузярова Кристина <kristina19283746@gmail.com>
2025-10-03 01:48:21 +0700

    Слэши в адресе

--- app.py
+++ app.py
@@ -309,3 +309,10 @@ def handle_500(err):
   <a href="/">На главную</a>
 </body>
 '''
+@app.route('/lab2/a')
+def a():
+    return 'без слэша'
+
+@app.route('/lab2/a/')
+def a2():
+    return 'со слэшом'

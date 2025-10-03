8c8251dba967bd9e3fad0aaa16ec1cab580f4ba5
Абузярова Кристина <kristina19283746@gmail.com>
2025-10-03 09:23:29 +0700

    Имя студента

--- app.py
+++ app.py
@@ -343,4 +343,5 @@ def add_flower(name):
 '''     
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Абузярова Кристина'
+    return render_template('example.html', name=name)

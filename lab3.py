0c518ed89f934345467231b13acaebca06ed40e1
Абузярова Кристина <kristina19283746@gmail.com>
2025-10-31 19:26:29 +0700

    правки

--- lab3.py
+++ lab3.py
@@ -295,3 +295,10 @@ def lab3():
     return render_template('lab3.html', 
                          name=name, 
                          name_color=name_color)
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)

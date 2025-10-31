b4b83ff9f144a64699fe76230c3776dbabee0fe5
Абузярова Кристина <kristina19283746@gmail.com>
2025-11-01 02:30:29 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -118,6 +118,15 @@ def tree():
 
     return redirect('/lab4/tree')
 
+users = [
+    {'login' : 'alex', 'password': '123'},
+    {'login' : 'Wow', 'password': '557'},
+    {'login' : 'krskask', 'password': '14567'},
+    {'login' : 'xopizzritochka', 'password': '0507'},
+    {'login' : 'alenkkkka', 'password': '1202'},
+    {'login' : 'mashkkaa', 'password': '2502'},
+]
+
 @lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -126,8 +135,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
     
     error = 'Неверный логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)

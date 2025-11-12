e1098e218bdf197663e76914fbbac5b487b2d3c8
Абузярова Кристина <kristina19283746@gmail.com>
2025-11-13 01:08:29 +0700

    Аутенцификация

--- lab5.py
+++ lab5.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, session
 lab5 = Blueprint('lab5', __name__)
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/lab5.html', username='anonymous')
+    return render_template('lab5/lab5.html', username=session.get('login', 'anonymous'))
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -55,4 +56,51 @@ def register():
     except psycopg2.OperationalError as e:
         return render_template('lab5/register.html', error=f'Ошибка подключения к БД: {str(e)}')
     except Exception as e:
-        return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
+         return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
+    
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    try:
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='postgres',  
+            user='postgres',      
+            password='postgres',  
+            port=5432
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        
+        cur.execute("SELECT * FROM users WHERE login = %s", (login,))
+        user = cur.fetchone()
+
+        if not user:
+            cur.close()
+            conn.close()
+            return render_template('lab5/login.html',
+                                error='Логин и/или пароль неверны')
+        
+        if user['password'] != password:
+            cur.close()
+            conn.close()
+            return render_template('lab5/login.html',
+                                error='Логин и/или пароль неверны')
+        
+        session['login'] = login
+        cur.close()
+        conn.close()
+        return render_template('lab5/success_login.html', login=login)
+    
+    except psycopg2.OperationalError as e:
+        return render_template('lab5/login.html', error=f'Ошибка подключения к БД: {str(e)}')
+    except Exception as e:
+        return render_template('lab5/login.html', error=f'Ошибка базы данных: {str(e)}')

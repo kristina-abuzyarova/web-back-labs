0eecb2f6fc680156b45da12554fddc022f4b6188
Абузярова Кристина <kristina19283746@gmail.com>
2025-11-27 08:47:28 +0700

    Заготовка для 6-й лабораторной работы

--- app.py
+++ app.py
@@ -7,20 +7,22 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
 app.secret_key = 'секретно-секретный секрет'
 load_dotenv()
 
-app.secret_key = os.getenv('SECRET_KEY', 'default-secret-key')
-app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+app.secret_key = 'your-secret-key-here'  
+app.config['DB_TYPE'] = 'postgres'  
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 access_log = []
 
 
@@ -387,6 +389,7 @@ def index():
                     <li><a href="/lab3">Третья лабораторная</a></li>
                     <li><a href="/lab4">Четвертая лабораторная</a></li>
                     <li><a href="/lab5">Пятая лабораторная</a></li>
+                    <li><a href="/lab6">Шестая лабораторная</a></li>
                 </ul>
             </nav>
         </main>

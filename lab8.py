49ada7374706a7feb3ac876fa04567de28c1d0f1
Абузярова Кристина <kristina19283746@gmail.com>
2025-12-24 20:31:46 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, redirect, session, curren
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from db import db
+from db.models import User, Article
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -15,9 +17,32 @@ def lab():
 def login():
     return render_template('lab8/login.html')
 
-@lab8.route('/lab8/register')
+@lab8.route('/lab8/register', methods=['GET', 'POST'])
 def register():
-    return render_template('lab8/register.html')
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form or not login_form.strip():
+        return render_template('lab8/register.html', error='Введите логин!')
+    if not password_form or not password_form.strip():
+        return render_template('lab8/register.html', error='Введите пароль!')
+
+    login_exists = User.query.filter_by(login=login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = User(login=login_form, password=password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    session['login'] = login_form
+    session['user_id'] = new_user.id
+    
+    return redirect('/lab8/')
 
 
 @lab8.route('/lab8/articles')
